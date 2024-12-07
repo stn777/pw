87e267c92deb186f237a96f4a338a55a47d2c6c3
stn777 <iserv@i.ua>
2024-12-07 20:33:57 +0000

    Delete

--- tests/test-2.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await page.getByRole('button', { name: 'Accept all' }).click();
-  
-});
